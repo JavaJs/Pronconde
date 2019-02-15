59ca293cf4314def1e10f8daa423c608ae5e1c30
JavaJs <47489643+JavaJs@users.noreply.github.com>
2019-02-15 12:03:29 -0600

    HumanDog.js
    This is a program that shows your age in dog years, you can assign your name and age to the variable myName and myAge to try it out for you.

---
+++ Human-Dog.js
@@ -0,0 +1,8 @@
+var myAge = 20;
+var myName = 'Anne';
+var dogYears = 7;
+console.log(myAge*dogYears);
+var myDogAge = 140;
+//you can change the dog age once you find your dog age: var myDogAge = 84;
+
+console.log(`${myName}, you are ${myAge}, your age in dog years is ${myDogAge} years old`);
